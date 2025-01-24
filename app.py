964b067118daf995df6b5d81765f9b1155f7313a
Declan Wong <ultradec@gmail.com>
2025-01-24 15:35:53 +1100

    Radioitems added with some styling

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import pandas
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, Input, Output, callback
 
 from plotly.express import line
 
@@ -10,23 +10,51 @@ dash_app = Dash(__name__)
 data = pandas.read_csv("./task2complete.csv")
 data = data.sort_values(by="date")
 
-line_chart = line(data, x="date", y="sales", title="Pink Morsel Sales")
-visualization = dcc.Graph(
-    id="visualization",
-    figure=line_chart
-)
+line_chart = line(data, x="date", y="sales", color="region", title="Pink Morsel Sales")
+
+
+region_colors = {
+    'north': '#1f77b4',  # Blue
+    'south': '#2ca02c',  # Green
+    'east': '#ff7f0e',   # Orange
+    'west': '#9467bd',   # Purple
+}
 
 header = html.H1(
     "Pink Morsel Sales",
-    id="header"
+    id="header",
+    style={"text-align": "center"}
 )
 
 dash_app.layout = html.Div(
     [
         header,
-        visualization
+        dcc.Graph(id="visualization", figure=line_chart),
+        dcc.RadioItems(
+            options=[
+                {'label': 'North', 'value': 'north'},
+                {'label': 'South', 'value': 'south'},
+                {'label': 'East', 'value': 'east'},
+                {'label': 'West', 'value': 'west'},
+                {'label': 'All Regions', 'value': 'all'}
+            ],
+            value='all',
+            id='options'
+        )
     ]
 )
+
+@callback(
+    Output('visualization', 'figure'),
+    Input('options', 'value'))
+def set_options(selected):
+    if selected == 'all':
+        filtered_data = data  
+    else:
+        filtered_data = data[data['region'] == selected]
+    color_column = "region"
+    updated_chart = line(filtered_data, x="date", y="sales", color=color_column, color_discrete_map=region_colors, title=f"Region: {selected.capitalize()}")
+    return updated_chart
 
 if __name__ == '__main__':
     dash_app.run_server()
